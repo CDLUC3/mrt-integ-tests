18efdb854036214420b8166c9bb684bccd519662
Terry Brady <terrywbrady@gmail.com>
2020-06-03 12:08:57 -0700

    Save config

--- spec/test/demo_spec.rb
+++ spec/test/demo_spec.rb
@@ -1,24 +1,42 @@
 require 'spec_helper.rb'
 require 'webdrivers/chromedriver'
 
-RSpec.describe ':demo', type: :feature do
+def load_config(name)
+  path = File.join('config', name)
+  raise Exception, "Config file #{name} not found!" unless File.exist?(path)
+  raise Exception, "Config file #{name} is empty!" if File.size(path) == 0
 
-  Capybara.app_host = 'https://merritt-stage.cdlib.org'
-  Capybara.run_server = false # don't start Rack
+  conf     = YAML.load_file(path)
+end
 
-  # TODO: why is this a good thing?
-  it 'value' do
-    foo = 'bar'
-    expect(foo).to eq('bar')
+RSpec.describe 'basic_merrit_ui_tests', type: :feature do
+
+  before(:each) do
+    conf = load_config('test_config.yml')
+    @test_config = conf['default']
+
+    Capybara.app_host = @test_config['url']
+    Capybara.run_server = false # don't start Rack
+
+    # @session = Capybara::Session.new(:selenium)
+    @session = Capybara::Session.new(:selenium_chrome_headless)
   end
 
-  it 'value2' do
-    # session = Capybara::Session.new(:selenium)
-    session = Capybara::Session.new(:selenium_chrome_headless)
-    session.visit 'https://merritt-stage.cdlib.org'
-    session.within "header" do
-      session.find_link('Login')
-      session.click_link('Login')
+  # TODO: why is this a good thing?
+  it 'Load Merritt UI home page' do
+    @session.visit '/'
+    @session.within("section.intro h2") do
+      expect(@session.text).to have_content("A trusted, cost-effective digital preservation repository")
     end
+  end
+
+  it 'Perform Merritt Guest Login' do
+    @session.visit '/'
+    @session.within "header" do
+      @session.find_link('Login')
+      @session.click_link('Login')
+    end
+    @session.find_button('Guest')
+    @session.click_button('Guest')
   end
 end
