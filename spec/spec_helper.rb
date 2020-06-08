ba2a82d17c368e17c235dfeefcd14574fc3d5c60
Terry Brady <terrywbrady@gmail.com>
2020-06-08 13:54:28 -0700

    Add use cases

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,45 @@ RSpec.configure do |config|
   config.formatter = :documentation
   config.include Capybara::DSL
 end
+
+def load_config(name)
+  path = File.join('config', name)
+  raise Exception, "Config file #{name} not found!" unless File.exist?(path)
+  raise Exception, "Config file #{name} is empty!" if File.size(path) == 0
+
+  conf     = YAML.load_file(path)
+end
+
+def get_config(key)
+  @test_config[key]
+end
+
+def create_web_session
+  conf = load_config('test_config.yml')
+  @test_config = conf['default']
+
+  Capybara.app_host = @test_config['url']
+  Capybara.run_server = false # don't start Rack
+
+  if ENV['CHROME_URL']
+    Capybara.register_driver :selenium_chrome_headless do |app|
+      args = [
+        '--no-default-browser-check',
+        '--start-maximized',
+        '--headless',
+        '--disable-dev-shm-usage',
+        '--whitelisted-ips'
+      ]
+      caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => args})
+
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :remote,
+        desired_capabilities: caps,
+        url: "http://chrome:4444/wd/hub"
+      )
+    end
+  end
+  @session = Capybara::Session.new(:selenium_chrome_headless)
+  #@session = Capybara::Session.new(:selenium_chrome)
+end
--- spec/test/demo_spec.rb
+++ spec/test/demo_spec.rb
@@ -1,44 +1,12 @@
 require 'spec_helper.rb'
 require 'webdrivers/chromedriver'
 
-def load_config(name)
-  path = File.join('config', name)
-  raise Exception, "Config file #{name} not found!" unless File.exist?(path)
-  raise Exception, "Config file #{name} is empty!" if File.size(path) == 0
-
-  conf     = YAML.load_file(path)
-end
 sleep 1
 
 RSpec.describe 'basic_merrit_ui_tests', type: :feature do
 
   before(:each) do
-    conf = load_config('test_config.yml')
-    @test_config = conf['default']
-
-    Capybara.app_host = @test_config['url']
-    Capybara.run_server = false # don't start Rack
-
-    if ENV['CHROME_URL']
-      Capybara.register_driver :selenium_chrome_headless do |app|
-        args = [
-          '--no-default-browser-check',
-          '--start-maximized',
-          '--headless',
-          '--disable-dev-shm-usage',
-          '--whitelisted-ips'
-        ]
-        caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => args})
-
-        Capybara::Selenium::Driver.new(
-          app,
-          browser: :remote,
-          desired_capabilities: caps,
-          url: "http://chrome:4444/wd/hub"
-        )
-      end
-    end
-    @session = Capybara::Session.new(:selenium_chrome_headless)
+    @session = create_web_session
   end
 
   # TODO: why is this a good thing?
@@ -58,4 +26,23 @@ RSpec.describe 'basic_merrit_ui_tests', type: :feature do
     @session.find_button('Guest')
     @session.click_button('Guest')
   end
+
+  it 'Open ark page' do
+    @session.visit '/'
+    @session.within "header" do
+      @session.find_link('Login')
+      @session.click_link('Login')
+    end
+    @session.find_button('Guest')
+    @session.click_button('Guest')
+
+    @session.visit "/m/#{get_config('guest_coll')}"
+    text = @session.find(:xpath, '//table/tbody/tr[1]/td[1]/a').text
+    @session.click_link(text)
+    @session.click_link("Version 1")
+    text = @session.find(:xpath, "//table[@class='properties'][2]/tbody/tr[2]/th[1]/a").text
+    @session.click_link(text)
+    expect(@session.current_url).to match(get_config('file_redirect_match'))
+  end
+
 end
