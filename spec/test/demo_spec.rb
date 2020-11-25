fb6eaa623dd331c2c7753b2e58fa59f9e48f1444
Terry Brady <terrywbrady@gmail.com>
2020-11-24 17:29:33 -0800

    check ingests

--- spec/test/demo_spec.rb
+++ spec/test/demo_spec.rb
@@ -6,6 +6,9 @@ sleep 1
 
 RSpec.describe 'basic_merrit_ui_tests', type: :feature do
   attr_reader :localid_prefix
+  before(:all) do
+    @localid_prefix = Time.new.strftime('%Y_%m_%d_%H%M') 
+  end
 
   def all_collections
     coll = [] 
@@ -20,7 +23,6 @@ RSpec.describe 'basic_merrit_ui_tests', type: :feature do
 
   before(:each) do
     @session = create_web_session
-    @localid_prefix = Time.new.strftime('%Y.%m.%d.%H%M') 
   end
 
   it 'Load Merritt UI home page' do
@@ -124,7 +126,7 @@ RSpec.describe 'basic_merrit_ui_tests', type: :feature do
       def add_file(fname, prefix, seq)
         path = create_filename(fname)
         f = create_file(path)
-        localid = "#{prefix}.#{seq}"
+        localid = "#{prefix}_#{seq}"
         title = localid
 
         @session.visit "/m/merritt_demo"
@@ -138,6 +140,7 @@ RSpec.describe 'basic_merrit_ui_tests', type: :feature do
         @session.within("section h1") do
           expect(@session.text).to have_content("Submission Received")
         end
+        sleep 5
       end
 
       it "Add README.md file" do
@@ -156,6 +159,54 @@ RSpec.describe 'basic_merrit_ui_tests', type: :feature do
         add_file('README %AF.md', localid_prefix, 'percent')
       end
 
+      it "Add README cliché.md file" do
+        add_file('README cliché.md', localid_prefix, 'accent')
+      end
+
+      it "sleep 30 to allow ingests to process..." do
+        sleep 30
+      end
+
+      def check_file(fname, prefix, seq)
+        localid = "#{prefix}_#{seq}"
+        title = localid
+
+        @session.visit "/m/merritt_demo"
+        @session.fill_in('terms', with: localid)
+        @session.find("input[name='commit']").click
+        @session.within("section h1") do
+          expect(@session.text).to have_content("Search Results")
+        end
+        text = @session.find(:xpath, "//table[@class='main']/tbody/tr[1]/th[@class='ark-header']/a").text
+        @session.click_link(text)
+        @session.within("section h2.object-title") do
+          expect(@session.text).to have_content(title)
+        end
+
+        @session.find_link(fname)
+        @session.click_link(fname)
+        expect(@session.status).to eq(200)
+      end
+
+      it "Check README.md file" do
+        check_file('README.md', localid_prefix, 'md')
+      end
+
+      it "Check README 1.md file" do
+        check_file('README 1.md', localid_prefix, 'space')
+      end
+
+      it "Check README+1.md file" do
+        check_file('README+1.md', localid_prefix, 'plus')
+      end
+
+      it "Check README %AF.md file" do
+        check_file('README %AF.md', localid_prefix, 'percent')
+      end
+
+      it "Check README cliché.md file" do
+        check_file('README cliché.md', localid_prefix, 'accent')
+      end
     end
   end
 
