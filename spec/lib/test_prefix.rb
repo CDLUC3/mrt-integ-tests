4ecef03aadfc3fa2c9cbedffe5af7da9a87cef80
Terry Brady <terrence.brady@ucop.edu>
2021-01-29 16:11:30 -0800

    missing lib code

---
+++ spec/lib/test_prefix.rb
@@ -0,0 +1,35 @@
+class TestObjectPrefix
+    @@localid_prefix = Time.new.strftime('%Y_%m_%d_%H%M')
+    @@config_file = File.join(Dir.getwd, 'config', 'test_config.yml')
+    @@integenv = ENV.fetch('INTEG_TEST_ENV', 'default')
+    @@config = Uc3Ssm::ConfigResolver.new({
+      def_value: 'N/A' 
+    }).resolve_file_values({
+      file: @@config_file, 
+      return_key: @@integenv
+    })
+  
+    def self.localid_prefix
+      @@localid_prefix
+    end
+  
+    def self.set_prefix(prefix)
+      @@localid_prefix = prefix
+    end
+  
+    def self.integ_test_environment
+      @@integenv
+    end
+
+    def self.get_yaml_config
+      @@config
+    end
+
+    def self.test_files
+      key = ENV.fetch('INGEST_FILES', 'default')
+      files = @@config.fetch('test-files', {})
+              .fetch(key, {})
+      return {} if files.nil?
+      files
+    end
+  end
