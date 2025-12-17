bdebe8cce9be61b372bd174915ae06cdf255e191
Terry Brady <terrywbrady@gmail.com>
2025-12-17 10:01:21 -0800

    set ui path for end to end tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
+# This script will launch tests when running as an ECS Task
+
+ui=$(aws servicediscovery discover-instances \
+          --service-name ui --namespace-name merritt-ecs-dev | \
+          jq -r ".Instances[0].Attributes.AWS_INSTANCE_IPV4")
+export PREVIEW_URL=http://${ui}:8086
+
 bundle exec rspec /spec/test
