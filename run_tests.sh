c6515a3f460f542264de884c59cfe21a99580b32
Terry Brady <terrywbrady@gmail.com>
2025-12-17 13:47:30 -0800

    refine start script

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,4 @@
 
 # This script will launch tests when running as an ECS Task
 
-ui=$(aws servicediscovery discover-instances \
-          --service-name ui --namespace-name merritt-ecs-dev | \
-          jq -r ".Instances[0].Attributes.AWS_INSTANCE_IPV4")
-export PREVIEW_URL=http://${ui}:8086
-
 bundle exec rspec /spec/test
