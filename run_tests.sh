196298cc964197f03d0b710358e7f7ebcc491660
Terry Brady <terrywbrady@gmail.com>
2026-05-19 16:06:59 -0700

    refine end to end output

--- run_tests.sh
+++ run_tests.sh
@@ -35,7 +35,7 @@ egrep "^(Finished in|[0-9]+ examples,)" $statfile.tmp > $statfile
 echo "" >> $statfile
 echo "To see a formatted version of the report, copy and paste the following URL into a browser:" >> $statfile
 echo "" >> $statfile
-echo "https://${baseurl}saved-reports/retrieve?report=reports%2F${rptfile}" >> $statfile
+echo "${baseurl}saved-reports/retrieve?report=reports%2F${rptfile}" >> $statfile
 echo "" >> $statfile
 cat $statfile.tmp >> $statfile
 
