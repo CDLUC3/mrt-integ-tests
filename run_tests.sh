98cc4a6d79a3faa2792da75c781c01b6460c6768
Terry Brady <terrywbrady@gmail.com>
2026-05-20 16:17:36 -0700

    slack message after int test

--- run_tests.sh
+++ run_tests.sh
@@ -34,6 +34,9 @@ echo "${baseurl}ops/s3-reports/retrieve?report=end2end%2F${rptfile}" >> $statfil
 echo "" >> $statfile
 cat $statfile.tmp >> $statfile
 
+export SLACK_BOT_TOKEN=$(aws ssm get-parameter --name "${SSM_ROOT_PATH}/slackbot" --with-decryption --query "Parameter.Value" --output text)
+ruby slack_message.rb $statfile
+
 if [ $FAIL -eq 1 ]
 then
   task_fail
