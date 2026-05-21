3fed0213980b8a4b713a41e8bb139c8d5ba5fe29
Terry Brady <terrywbrady@gmail.com>
2026-05-21 16:21:05 -0700

    timing adjust

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -115,7 +115,7 @@ def authenticated_login
     @session.click_link('Login')
   end
 
-  sleep 1
+  sleep 2
   @session.fill_in('login', with: login_user)
   @session.fill_in('password', with: login_password)
   @session.find('#submit_login').click
