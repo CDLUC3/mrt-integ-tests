748a911297a7ff3524d9e3b875c81f8d33360d70
Terry Brady <terrywbrady@gmail.com>
2026-05-20 18:09:21 -0700

    simplify output

--- run_tests.sh
+++ run_tests.sh
@@ -24,11 +24,11 @@ set +o pipefail
 rptfile="$(date +%Y%m%d-%H%M%S).txt"
 aws s3 cp $statfile "s3://${S3REPORT_BUCKET}/end2end/${rptfile}"
 
-egrep "^(Finished in|[0-9]+ examples,)" $statfile.tmp > $statfile
+egrep "^(Finished in|[0-9]+ examples,)" $statfile > $statfile.slack
 echo "" >> $statfile.slack
 echo "To see a formatted version of the report, copy and paste the following URL into a browser:" >> $statfile.slack
 echo "" >> $statfile.slack
-echo "${baseurl}ops/s3-reports/retrieve?report=end2end%2F${rptfile}" >> $statfile
+echo "${baseurl}ops/s3-reports/retrieve?report=end2end%2F${rptfile}" >> $statfile.slack
 
 export SLACK_BOT_TOKEN=$(aws ssm get-parameter --name "${SLACK_BOT_SSM}" --with-decryption --query "Parameter.Value" --output text)
 ruby slack_message.rb $statfile.slack
