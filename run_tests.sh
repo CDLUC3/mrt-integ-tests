a26ca2c0ec50151c94521944399331d3fd9e5610
Terry Brady <terrywbrady@gmail.com>
2026-05-20 18:07:54 -0700

    simplify output

--- run_tests.sh
+++ run_tests.sh
@@ -24,26 +24,18 @@ set +o pipefail
 rptfile="$(date +%Y%m%d-%H%M%S).txt"
 aws s3 cp $statfile "s3://${S3REPORT_BUCKET}/end2end/${rptfile}"
 
-mv $statfile $statfile.tmp
-
 egrep "^(Finished in|[0-9]+ examples,)" $statfile.tmp > $statfile
-echo "" >> $statfile
-echo "To see a formatted version of the report, copy and paste the following URL into a browser:" >> $statfile
-echo "" >> $statfile
+echo "" >> $statfile.slack
+echo "To see a formatted version of the report, copy and paste the following URL into a browser:" >> $statfile.slack
+echo "" >> $statfile.slack
 echo "${baseurl}ops/s3-reports/retrieve?report=end2end%2F${rptfile}" >> $statfile
-echo "" >> $statfile
-echo "" >> $statfile
-echo '```' >> $statfile
-cat $statfile.tmp >> $statfile
-echo "" >> $statfile
-echo '```' >> $statfile
 
 export SLACK_BOT_TOKEN=$(aws ssm get-parameter --name "${SLACK_BOT_SSM}" --with-decryption --query "Parameter.Value" --output text)
-ruby slack_message.rb $statfile
+ruby slack_message.rb $statfile.slack
 
 if [ $FAIL -eq 1 ]
 then
-  task_fail
+  task_fail N
 else
   task_complete
 fi
