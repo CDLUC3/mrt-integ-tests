2462e3f92c7e2477059712e97bf8dc921fae92b1
Terry Brady <terrywbrady@gmail.com>
2026-06-12 12:32:13 -0700

    gem updates

--- slack_message.rb
+++ slack_message.rb
@@ -1,20 +1,22 @@
-require "net/http"
-require "json"
-require "uri"
+# frozen_string_literal: true
 
-token = ENV.fetch("SLACK_BOT_TOKEN")       # xoxb-...
-channel = ENV.fetch("SLACK_CHANNELID")    # e.g. C0123456789
+require 'net/http'
+require 'json'
+require 'uri'
 
-uri = URI("https://slack.com/api/chat.postMessage")
+token = ENV.fetch('SLACK_BOT_TOKEN') # xoxb-...
+channel = ENV.fetch('SLACK_CHANNELID') # e.g. C0123456789
+
+uri = URI('https://slack.com/api/chat.postMessage')
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
 
-message = File.read(ARGV[0] || "*no data* provided")
+message = File.read(ARGV[0] || '*no data* provided')
 icon_emoji = ARGV[1] || ':merritt:'
 
 request = Net::HTTP::Post.new(uri)
-request["Authorization"] = "Bearer #{token}"
-request["Content-Type"] = "application/json; charset=utf-8"
+request['Authorization'] = "Bearer #{token}"
+request['Content-Type'] = 'application/json; charset=utf-8'
 request.body = {
   channel: channel,
   mrkdwn: true,
@@ -25,8 +27,8 @@ request.body = {
 response = http.request(request)
 body = JSON.parse(response.body)
 
-if body["ok"]
-  puts "Message sent. ts=#{body["ts"]}"
+if body['ok']
+  puts "Message sent. ts=#{body['ts']}"
 else
-  warn "Slack API error: #{body["error"]}"
+  warn "Slack API error: #{body['error']}"
 end
